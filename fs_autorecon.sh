138af72208b4dfff343b8313a5de4cfdbeb67422
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2017-11-05 18:51:30 +0900

    enable parallel mode for recon-all

--- fs_autorecon.sh
+++ fs_autorecon.sh
@@ -4,27 +4,31 @@
 #Usage: fs_autorecon.sh <nifti files>
 #Wild card can be used
 
-#26-Sep-2017 K.Nemoto
+#05-Nov-2017 K.Nemoto
 
-##Check OS
-#os=$(uname)
-#
-##Check number of cores (threads)
-#if [ $os == "Linux" ]
-#then
-#    ncores=$(nproc)
-#elif [ $os == "Darwin" ]
-#then 
-#    ncores=$(sysctl -n hw.ncpu)
-#else
-#    echo "Cannot detect your OS!"
-#    exit 1
-#fi
-#
-#echo "Your logical cores are ${ncores}"
-#
-##Decide how many cores to use for freesurfer ($ncores -1)
-#fscore=$(expr $ncores -1)
+#Check OS
+os=$(uname)
+
+#Check number of cores (threads)
+if [ $os == "Linux" ]; then
+    ncores=$(nproc)
+elif [ $os == "Darwin" ]; then 
+    ncores=$(sysctl -n hw.ncpu)
+else
+    echo "Cannot detect your OS!"
+    exit 1
+fi
+
+echo "Your logical cores are ${ncores}"
+
+#If logical cores >=4, enable parallelize option
+if [ $ncores -ge 4 ]; then
+	parallel="-parallel"
+	echo "enable parallel mode"
+else
+	parallel=""
+	echo "disable parallel mode"
+fi
 
 if [ $# -lt 1 ]
 then
@@ -36,16 +40,9 @@ fi
 
 echo "Begin recon-all"
 
-#count=0
-
 for f in "$@"
 do
 	subjid=${f%%.*}
-	recon-all -i $f -s $subjid -all
-
-#	count=$(echo "$count + 1" | bc)
-#	if [ $(echo "$count % $fscore" | bc) -eq 0 ]; then
-#	  wait
-#	fi
+	recon-all -i $f -s $subjid -all $parallel
 done
 
